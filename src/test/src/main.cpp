b54fc6d054059c5367cee3db9ea2ac75a70dfa9d
skywalker <skywalker.dell@gmail.com>
2022-08-03 13:16:00 +0800

    modify setting.json & modify .gitignore

--- src/test/src/main.cpp
+++ src/test/src/main.cpp
@@ -1,14 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  /*产生一个Wang2的节点*/
-  auto node = std::make_shared<rclcpp::Node>("wang2");
-  // 打印一句自我介绍
-  RCLCPP_INFO(node->get_logger(), "hello world.");
-  /* 运行节点，并检测退出信号*/
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    /*产生一个Wang2的节点*/
+    auto node = std::make_shared<rclcpp::Node>("wang2");
+    // 打印一句自我介绍
+    RCLCPP_INFO(node->get_logger(), "hello world.");
+    /* 运行节点，并检测退出信号*/
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
